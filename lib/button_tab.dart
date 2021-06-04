3b31719f18e0c4c7a4f1753b2747d8946477377f
ukieTux <hey.mudassir@gmail.com>
2021-06-04 11:48:04 +0800

    Fix margin issue on last item

--- lib/button_tab.dart
+++ lib/button_tab.dart
@@ -51,53 +51,54 @@ class _ButtonsTabState extends State<ButtonsTab> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: widget.width ?? widthInPercent(100, context),
-      height: widget.height ?? 50,
-      margin: widget.isSelected! ? widget.marginSelected : EdgeInsets.zero,
-      // enable decoration when item is selected
-      decoration: widget.isSelected!
-          ? bdHeader.copyWith(
-              borderRadius: BorderRadius.circular(widget.radius!),
-              gradient: LinearGradient(
-                // Where the linear gradient begins and ends
-                begin: widget.begin ?? Alignment.topCenter,
-                end: widget.end ?? Alignment.bottomCenter,
-                colors:
-                    widget.selectedColors ?? [Theme.of(context).primaryColor],
-              ))
-          : null,
-      child: TextButton(
-          onPressed: widget.onPressed as void Function()?,
-          style: ButtonStyle(
-              shape: MaterialStateProperty.all(new RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(widget.radius!))),
-              padding: MaterialStateProperty.all(EdgeInsets.zero)),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              widget.icons != null
-                  ? Icon(
-                      widget.icons,
-                      color: widget.isSelected!
-                          ? widget.selectedTextStyle!.color
-                          : widget.unSelectedTextStyle!.color,
-                    )
-                  : Container(),
-              Visibility(
-                visible: widget.icons != null,
-                child: SizedBox(
-                  width: 4,
-                ),
-              ),
-              Text(
-                widget.title!,
-                style: widget.isSelected!
-                    ? widget.selectedTextStyle
-                    : widget.unSelectedTextStyle,
-                textAlign: TextAlign.center,
-              )
-            ],
-          )),
-    );
+        width: widget.width ?? widthInPercent(100, context),
+        height: widget.height ?? 50,
+        //wrap with container to fix margin issue
+        child: Container(
+          margin: widget.isSelected! ? widget.marginSelected : EdgeInsets.zero,
+          decoration: widget.isSelected!
+              ? bdHeader.copyWith(
+                  borderRadius: BorderRadius.circular(widget.radius!),
+                  gradient: LinearGradient(
+                    // Where the linear gradient begins and ends
+                    begin: widget.begin ?? Alignment.topCenter,
+                    end: widget.end ?? Alignment.bottomCenter,
+                    colors: widget.selectedColors ??
+                        [Theme.of(context).primaryColor],
+                  ))
+              : null,
+          child: TextButton(
+              onPressed: widget.onPressed as void Function()?,
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all(new RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(widget.radius!))),
+                  padding: MaterialStateProperty.all(EdgeInsets.zero)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  widget.icons != null
+                      ? Icon(
+                          widget.icons,
+                          color: widget.isSelected!
+                              ? widget.selectedTextStyle!.color
+                              : widget.unSelectedTextStyle!.color,
+                        )
+                      : Container(),
+                  Visibility(
+                    visible: widget.icons != null,
+                    child: SizedBox(
+                      width: 4,
+                    ),
+                  ),
+                  Text(
+                    widget.title!,
+                    style: widget.isSelected!
+                        ? widget.selectedTextStyle
+                        : widget.unSelectedTextStyle,
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              )),
+        ));
   }
 }
