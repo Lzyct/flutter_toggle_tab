fbc63203e1d5ae9bd6a267c97e6a627df73a9052
Lzyct <hey.mudassir@gmail.com>
2025-01-05 23:47:33 +0800

    chore: update example with the latest library version

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_toggle_tab/flutter_toggle_tab.dart';
-import 'package:flutter_toggle_tab/helper.dart';
 
 void main() {
   runApp(MyApp());
@@ -35,15 +34,13 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Toggle Tab Sample Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -51,24 +48,57 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final ValueNotifier<int> _tabIndexBasicToggle = ValueNotifier(1);
+  final ValueNotifier<int> _tabIndexBasicToggleCounter = ValueNotifier(1);
   final ValueNotifier<int> _tabIndexTextWithIcon = ValueNotifier(0);
   final ValueNotifier<int> _tabIndexIconButton = ValueNotifier(0);
   final ValueNotifier<int> _tabIndexUpdateProgrammatically = ValueNotifier(0);
 
-  List<String> get _listTextTabToggle =>
-      ["Tab A (10)", "Tab B (6)", "Tab C (9)"];
-
-  List<String> get _listTextSelectedToggle =>
-      ["Select A (10)", "Select B (6)", "Select C (9)"];
-
-  List<IconData> get _listIconTabToggle => [
-        Icons.person,
-        Icons.pregnant_woman,
+  List<DataTab> get _listTextTabToggle => [
+        DataTab(title: "Tab A (10)"),
+        DataTab(title: "Tab B (5)"),
+        DataTab(title: "Tab C (19)"),
       ];
 
-  List<String> get _listGenderText => ["Male", "Female"];
+  late final _listTextTabToggleCounter = [
+    DataTab(
+      title: "Tab A",
+      counterWidget: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: DecoratedBox(
+          decoration:
+              const BoxDecoration(color: Colors.red, shape: BoxShape.circle),
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: Text(
+              "1",
+              style: Theme.of(context)
+                  .textTheme
+                  .labelSmall
+                  ?.copyWith(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    ),
+    DataTab(title: "Tab B (5)"),
+    DataTab(title: "Tab C (19)"),
+  ];
 
-  List<String> get _listGenderEmpty => ["", ""];
+  List<DataTab> get _listTextSelectedToggle => [
+        DataTab(title: "Select A (10)"),
+        DataTab(title: "Select B (5)"),
+        DataTab(title: "Select C (19)"),
+      ];
+
+  List<DataTab> get _listIconTabToggle => [
+        DataTab(icon: Icons.person),
+        DataTab(icon: Icons.pregnant_woman),
+      ];
+
+  List<DataTab> get _listGenderText => [
+        DataTab(title: "Male", icon: Icons.person),
+        DataTab(title: "Female", icon: Icons.pregnant_woman),
+      ];
 
   @override
   Widget build(BuildContext context) {
@@ -81,12 +111,14 @@ class _MyHomePageState extends State<MyHomePage> {
         padding: const EdgeInsets.symmetric(vertical: 36, horizontal: 8),
         child: Column(
           children: <Widget>[
-            basicTabToggle(),
-            ...divider(),
+            _basicTabToggle(),
+            ..._divider(),
+            _basicTabToggleWithCounter(),
+            ..._divider(),
             textWithIcon(),
-            ...divider(),
+            ..._divider(),
             iconButton(),
-            ...divider(),
+            ..._divider(),
             updateProgrammatically(),
           ],
         ),
@@ -95,13 +127,13 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  List<Widget> divider() => [
+  List<Widget> _divider() => [
         SizedBox(height: heightInPercent(3, context)),
         const Divider(thickness: 2),
         SizedBox(height: heightInPercent(3, context)),
       ];
 
-  Widget basicTabToggle() => Column(
+  Widget _basicTabToggle() => Column(
         children: [
           /// Basic Toggle Sample
           SizedBox(height: heightInPercent(3, context)),
@@ -133,7 +165,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   fontSize: 14,
                   fontWeight: FontWeight.w500,
                 ),
-                labels: _listTextTabToggle,
+                dataTab: _listTextTabToggle,
                 selectedLabelIndex: (index) {
                   _tabIndexBasicToggle.value = index;
                 },
@@ -146,6 +178,65 @@ class _MyHomePageState extends State<MyHomePage> {
               _tabIndexBasicToggle.value = 2;
             },
             child: const Text("Change to Index 2"),
+          ),
+          SizedBox(height: heightInPercent(3, context)),
+          ValueListenableBuilder(
+            valueListenable: _tabIndexBasicToggle,
+            builder: (context, currentIndex, _) {
+              return Text(
+                "Index selected : $currentIndex",
+                style: const TextStyle(fontSize: 20),
+              );
+            },
+          ),
+        ],
+      );
+
+  Widget _basicTabToggleWithCounter() => Column(
+        children: [
+          /// Basic Toggle Sample
+          SizedBox(height: heightInPercent(3, context)),
+          const Text(
+            "Basic Tab Toggle with Counter Widget",
+            style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+          ),
+          SizedBox(height: heightInPercent(3, context)),
+          ValueListenableBuilder(
+            valueListenable: _tabIndexBasicToggleCounter,
+            builder: (context, currentIndex, _) {
+              return FlutterToggleTab(
+                // width in percent
+                width: 90,
+                borderRadius: 30,
+                height: 50,
+                selectedIndex: currentIndex,
+                selectedBackgroundColors: const [
+                  Colors.blue,
+                  Colors.blueAccent,
+                ],
+                selectedTextStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                ),
+                unSelectedTextStyle: const TextStyle(
+                  color: Colors.black87,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+                dataTab: _listTextTabToggleCounter,
+                selectedLabelIndex: (index) {
+                  _tabIndexBasicToggleCounter.value = index;
+                },
+                isScroll: false,
+              );
+            },
+          ),
+          TextButton(
+            onPressed: () {
+              _tabIndexBasicToggleCounter.value = 0;
+            },
+            child: const Text("Change to Index 0"),
           ),
           SizedBox(height: heightInPercent(3, context)),
           ValueListenableBuilder(
@@ -193,8 +284,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         fontSize: 14,
                         fontWeight: FontWeight.w400,
                       ),
-                      labels: _listGenderText,
-                      icons: _listIconTabToggle,
+                      dataTab: _listGenderText,
                       selectedIndex: currentIndex,
                       selectedLabelIndex: (index) {
                         _tabIndexTextWithIcon.value = index;
@@ -212,7 +302,7 @@ class _MyHomePageState extends State<MyHomePage> {
             valueListenable: _tabIndexTextWithIcon,
             builder: (context, currentIndex, _) {
               return Text(
-                "Selected sex : ${_listGenderText[currentIndex]} ",
+                "Selected sex : ${_listGenderText[currentIndex].title} ",
                 style: const TextStyle(fontSize: 20),
               );
             },
@@ -254,8 +344,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         fontSize: 14,
                         fontWeight: FontWeight.w400,
                       ),
-                      labels: _listGenderEmpty,
-                      icons: _listIconTabToggle,
+                      dataTab: _listIconTabToggle,
                       iconSize: 40,
                       selectedLabelIndex: (index) {
                         _tabIndexIconButton.value = index;
@@ -315,7 +404,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         fontSize: 14,
                         fontWeight: FontWeight.w400,
                       ),
-                      labels: _listTextSelectedToggle,
+                      dataTab: _listTextSelectedToggle,
                       selectedLabelIndex: (index) {
                         _tabIndexUpdateProgrammatically.value = index;
                       },
@@ -327,19 +416,17 @@ class _MyHomePageState extends State<MyHomePage> {
                     _tabIndexUpdateProgrammatically.value = 2;
                   },
                   child: const Text("Select C"),
-                )
+                ),
               ],
             ),
           ),
           SizedBox(height: heightInPercent(3, context)),
           ValueListenableBuilder(
             valueListenable: _tabIndexUpdateProgrammatically,
-            builder: (context, currentIndex, _) {
-              return Text(
-                "Selected sex index: $currentIndex ",
-                style: const TextStyle(fontSize: 20),
-              );
-            },
+            builder: (context, currentIndex, _) => Text(
+              "Selected sex index: $currentIndex ",
+              style: const TextStyle(fontSize: 20),
+            ),
           ),
         ],
       );
